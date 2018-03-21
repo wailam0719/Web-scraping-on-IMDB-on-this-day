5f025429a20b7c4d8c7765e2716ca410ade2ef7f
wailam0719 <37608427+wailam0719@users.noreply.github.com>
2018-03-21 12:51:57 +0800

    Add files via upload

---
+++ web_multiple_testing_1.py
@@ -0,0 +1,56 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import csv
+
+def movie_birth(url_birth):
+    page_birth = requests.get(url_birth)
+    soup_birth = BeautifulSoup(page_birth.content, 'html.parser')
+    listers = soup_birth.findAll("div", class_="lister-item mode-detail")
+    birth_list=[]
+
+    for lister in listers:
+        image_lister = lister.findAll("div", class_="lister-item-image")
+        image = image_lister[0].img["src"]
+        birth = [image]
+        name_lister = lister.findAll("h3",class_="lister-item-header")
+        name = name_lister[0].a.text.strip()
+        birth.append(name)
+        details_lister = lister.findAll("p",class_="text-muted text-small")
+        details = []
+        for detail in details_lister:
+            details.append(detail.text.strip())
+            for i in details:
+                i = i.split(' |\n ')
+                if len(i) == 2:
+                    profession = i[0]
+                    birth.append(profession)
+                    bestwork = i[1]
+                    birth.append(bestwork)
+                else:
+                    profession = 'null'
+                    birth.append(profession)
+                    bestwork = i[0]
+                    birth.append(bestwork)
+                birth_list.append(birth)
+
+    return birth_list
+   
+url_front = "https://www.imdb.com/search/name?birth_monthday="
+
+for month in range(1,13):
+    for day in range(1,32):
+         month_2d  = '{:02d}'.format(month)
+         day_2d = '{:02d}'.format(day)
+         url_birth = url_front + str(month_2d) + "-" + str(day_2d)
+            
+         print(url_birth)
+         content = movie_birth(url_birth)
+         if len(content) > 0:
+                filepath = month_2d+day_2d+".csv"
+                
+                with open(filepath, 'w', newline='') as csvfile:
+                    mywriter = csv.writer(csvfile)
+                    mywriter.writerows(content)   
+                    print(filepath,"saved.")
+                  
